1091b07efda8b8207c228e60f28ed13d5ab5bc20
abdojat <3oooo3.2003@gmail.com>
2025-09-20 19:51:18 +0300

    fixing a bug

--- src/components/layout/MetaData.js
+++ src/components/layout/MetaData.js
@@ -1,11 +1,27 @@
-import { Helmet } from 'react-helmet';
+import * as HelmetModule from 'react-helmet';
+
+// Support either named or default Helmet exports from react-helmet
+const Helmet = HelmetModule.Helmet || HelmetModule.default || null;
 
 const MetaData = ({ title }) => {
-    return (
-        <Helmet>
-            <title>{`${title} - AbadyzShop`}</title>
-        </Helmet>
-    );
+    const fullTitle = `${title} - AbadyzShop`;
+
+    // If Helmet is available as a valid React component, use it.
+    if (Helmet && typeof Helmet === 'function') {
+        return (
+            <Helmet>
+                <title>{fullTitle}</title>
+            </Helmet>
+        );
+    }
+
+    // Fallback: directly set document.title to avoid rendering errors in
+    // case the Helmet import is an object or not available in the runtime.
+    if (typeof document !== 'undefined') {
+        document.title = fullTitle;
+    }
+
+    return null;
 };
 
 export default MetaData;
